849d8db8aef37a57fcf31b69d294d964d39b5db5
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2020-11-18 13:00:42 +0100

    JCES-1723: Fix SUT in DualConnectionPerfTest
    The test was dominated by Mockito. Never use Mockito for perf tests.
    Mockito is really slow.

--- src/test/java/com/atlassian/db/replica/api/DualConnectionPerfTest.java
+++ src/test/java/com/atlassian/db/replica/api/DualConnectionPerfTest.java
@@ -1,7 +1,8 @@
 package com.atlassian.db.replica.api;
 
-import com.atlassian.db.replica.api.mocks.ConnectionProviderMock;
+import com.atlassian.db.replica.api.mocks.NoOpConnection;
 import com.atlassian.db.replica.api.mocks.PermanentConsistency;
+import com.atlassian.db.replica.spi.ConnectionProvider;
 import org.junit.Test;
 
 import java.sql.Connection;
@@ -13,28 +14,50 @@ import static com.atlassian.db.replica.api.Queries.LARGE_SQL_QUERY;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DualConnectionPerfTest {
-    private final ConnectionProviderMock connectionProvider = new ConnectionProviderMock();
 
     @Test
     public void shouldHaveAcceptableThruput() throws SQLException {
-        final DualConnection connection = DualConnection.builder(connectionProvider, new PermanentConsistency()).build();
-        final int times = 100000;
+        final DualConnection connection = DualConnection
+            .builder(
+                new NoOpConnectionProvider(),
+                new PermanentConsistency()
+            ).build();
+        final int times = 100000000;
 
         final Duration duration = runBenchmark(connection, times);
 
         float thruputPerMillis = (float) times / duration.toMillis();
-        float thruputPerSecond = thruputPerMillis * 1000;
-        assertThat(thruputPerSecond)
-            .as("thruput per second")
-            .isGreaterThan(10_000);
+        assertThat(thruputPerMillis)
+            .as("thruput per ms")
+            .isGreaterThan(5_000);
     }
 
     private Duration runBenchmark(Connection connection, int times) throws SQLException {
         final Instant start = Instant.now();
+        int hashCode = 0;
         for (int i = 0; i < times; i++) {
-            connection.prepareStatement(LARGE_SQL_QUERY).executeQuery().hashCode();
+            hashCode += connection.prepareStatement(LARGE_SQL_QUERY).executeQuery().hashCode();
         }
+        System.out.println("I really need that number. JIT gods don't kill my code paths. " + hashCode);
         return Duration.between(start, Instant.now());
+    }
+
+    private static class NoOpConnectionProvider implements ConnectionProvider {
+
+        @Override
+        public boolean isReplicaAvailable() {
+            return true;
+        }
+
+        @Override
+        public Connection getMainConnection() {
+            return new NoOpConnection();
+        }
+
+        @Override
+        public Connection getReplicaConnection() {
+            return new NoOpConnection();
+        }
     }
 
 }
