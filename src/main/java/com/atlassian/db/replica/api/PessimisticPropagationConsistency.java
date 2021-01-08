88ac9cf1db05529bf7311ecf6e2b950c7caccfb5
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2021-01-08 16:53:21 +0100

    Replace `PessmisticPropagationConsistency` static factory method with a builder

--- src/main/java/com/atlassian/db/replica/api/PessimisticPropagationConsistency.java
+++ src/main/java/com/atlassian/db/replica/api/PessimisticPropagationConsistency.java
@@ -1,10 +1,13 @@
-package com.atlassian.db.replica.internal;
+package com.atlassian.db.replica.api;
 
-import com.atlassian.db.replica.spi.*;
+import com.atlassian.db.replica.internal.MonotonicMemoryCache;
+import com.atlassian.db.replica.spi.Cache;
+import com.atlassian.db.replica.spi.ReplicaConsistency;
 
-import java.sql.*;
-import java.time.*;
-import java.util.Optional;
+import java.sql.Connection;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.function.Supplier;
 
 /**
@@ -17,7 +20,44 @@ public class PessimisticPropagationConsistency implements ReplicaConsistency {
     private final Duration maxPropagation;
     private final Cache<Instant> lastWrite;
 
-    public PessimisticPropagationConsistency(Clock clock, Duration maxPropagation, Cache<Instant> lastWrite) {
+    public static class Builder {
+        private Duration maxPropagation = Duration.ofMillis(100);
+        private Cache<Instant> lastWrite = new MonotonicMemoryCache<>();
+        private Clock clock = Clock.systemUTC();
+
+        /**
+         * @param maxPropagation how long do writes propagate from main to replica
+         */
+        public Builder assumeMaxPropagation(Duration maxPropagation) {
+            this.maxPropagation = maxPropagation;
+            return this;
+        }
+
+        /**
+         * @param lastWrite remembers last write
+         */
+        public Builder cacheLastWrite(Cache<Instant> lastWrite) {
+            this.lastWrite = lastWrite;
+            return this;
+        }
+
+        /**
+         * @param clock measures flow of time
+         */
+        public Builder measureTime(Clock clock) {
+            this.clock = clock;
+            return this;
+        }
+
+        /**
+         * @return consistency assuming consistency after max propagation since last write (if known)
+         */
+        public ReplicaConsistency build() {
+            return new PessimisticPropagationConsistency(clock, maxPropagation, lastWrite);
+        }
+    }
+
+    private PessimisticPropagationConsistency(Clock clock, Duration maxPropagation, Cache<Instant> lastWrite) {
         this.clock = clock;
         this.maxPropagation = maxPropagation;
         this.lastWrite = lastWrite;
@@ -45,8 +85,9 @@ public class PessimisticPropagationConsistency implements ReplicaConsistency {
     /**
      * If {@code lastWrite} is unknown, assume the write just happened, e.g. it didn't propagate yet.
      * This assumption errs on the side of caution: more true inconsistencies at the cost of fewer true consistencies.
-     *
+     * <p>
      * Propagates write assumption to the cache. This prevents from assuming write just happened until the next write.
+     *
      * @return known or assumed time of last write
      */
     private Instant assumeLastWrite() {
