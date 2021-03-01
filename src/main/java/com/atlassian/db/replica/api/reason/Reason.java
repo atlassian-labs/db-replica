989c6b01a5a6cd6b1ff4331332ef611eecb8fa41
Artur Faruga <afaruga@atlassian.com>
2021-03-01 15:50:50 +0100

    JCES-1896 Adding isWrite information to API - added isWrite information on compile lvl to Reason and make it main source of truth for isWriteOperation. Introduced SqlQuery with OO goodness. Only (RW_API_CALL, WRITE_OPERATION) are Write reasons, rest is Read reasons, but might be run on MAIN due to connection state and other condtions

--- src/main/java/com/atlassian/db/replica/api/reason/Reason.java
+++ src/main/java/com/atlassian/db/replica/api/reason/Reason.java
@@ -8,33 +8,41 @@ import java.util.Objects;
 public final class Reason {
     private final String name;
     private final boolean isRunOnMain;
+    private final boolean isWrite;
 
-    private Reason(final String name, boolean isRunOnMain) {
+    private Reason(final String name, boolean isRunOnMain, boolean isWrite) {
         this.name = name;
         this.isRunOnMain = isRunOnMain;
+        this.isWrite = isWrite;
     }
 
-    public static final Reason RW_API_CALL = new Reason("RW_API_CALL", true);
-    public static final Reason REPLICA_INCONSISTENT = new Reason("REPLICA_INCONSISTENT", true);
-    public static final Reason READ_OPERATION = new Reason("READ_OPERATION", false);
-    public static final Reason WRITE_OPERATION = new Reason("WRITE_OPERATION", true);
-    public static final Reason LOCK = new Reason("LOCK", true);
-    public static final Reason MAIN_CONNECTION_REUSE = new Reason(
-        "MAIN_CONNECTION_REUSE",
-        true
-    );
-    public static final Reason HIGH_TRANSACTION_ISOLATION_LEVEL = new Reason(
-        "HIGH_TRANSACTION_ISOLATION_LEVEL",
-        true
-    );
-    public static final Reason RO_API_CALL = new Reason("RO_API_CALL", false);
+    public static final Reason RW_API_CALL =
+            new ReasonBuilder("RW_API_CALL").isRunOnMain(true).isWrite(true).build();
+    public static final Reason REPLICA_INCONSISTENT =
+            new ReasonBuilder("REPLICA_INCONSISTENT").isRunOnMain(true).isWrite(false).build();
+    public static final Reason READ_OPERATION =
+            new ReasonBuilder("READ_OPERATION").isRunOnMain(false).isWrite(false).build();
+    public static final Reason WRITE_OPERATION =
+            new ReasonBuilder("WRITE_OPERATION").isRunOnMain(true).isWrite(true).build();
+    public static final Reason LOCK =
+            new ReasonBuilder("LOCK").isRunOnMain(true).isWrite(false).build();
+    public static final Reason MAIN_CONNECTION_REUSE =
+            new ReasonBuilder("MAIN_CONNECTION_REUSE").isRunOnMain(true).isWrite(false).build();
+    public static final Reason HIGH_TRANSACTION_ISOLATION_LEVEL =
+            new ReasonBuilder("HIGH_TRANSACTION_ISOLATION_LEVEL").isRunOnMain(true).isWrite(false).build();
+    public static final Reason RO_API_CALL =
+            new ReasonBuilder("RO_API_CALL").isRunOnMain(false).isWrite(false).build();
 
     public String getName() {
         return name;
     }
 
-    public boolean isRunOnMain() {
+    boolean isRunOnMain() {
         return isRunOnMain;
+    }
+
+    boolean isWrite() {
+        return isWrite;
     }
 
     @Override
@@ -42,7 +50,7 @@ public final class Reason {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Reason reason = (Reason) o;
-        return isRunOnMain == reason.isRunOnMain && Objects.equals(name, reason.name);
+        return isRunOnMain == reason.isRunOnMain && isWrite == reason.isWrite && Objects.equals(name, reason.name);
     }
 
     @Override
@@ -55,6 +63,31 @@ public final class Reason {
         return "Reason{" +
             "name='" + name + '\'' +
             ", isRunOnMain=" + isRunOnMain +
+            ", isWrite=" + isWrite +
             '}';
+    }
+
+    private static class ReasonBuilder {
+        private final String name;
+        private boolean isRunOnMain = false;
+        private boolean isWrite = false;
+
+        ReasonBuilder(final String name) {
+            this.name = name;
+        }
+        
+        ReasonBuilder isRunOnMain(boolean isRunOnMain) {
+            this.isRunOnMain = isRunOnMain;
+            return this;
+        }
+
+        ReasonBuilder isWrite(boolean isWrite) {
+            this.isWrite = isWrite;
+            return this;
+        }
+
+        Reason build() {
+            return new Reason(name, isRunOnMain, isWrite);
+        }
     }
 }
