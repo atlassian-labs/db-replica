406f14ad41b49fbabc2bbd3d855499b0f5075e6a
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2021-08-18 15:31:51 +0200

    Remove ClusterConsistency Remove ReplicaNodeAwareConnectionProvider Remove AuroraCluster to AuroraClusterDiscovery Move AuroraClusterDiscovery to internal

--- src/test/java/com/atlassian/db/replica/it/example/aurora/replica/api/MultiReplicaConsistency.java
+++ src/test/java/com/atlassian/db/replica/it/example/aurora/replica/api/MultiReplicaConsistency.java
@@ -1,23 +1,23 @@
 package com.atlassian.db.replica.it.example.aurora.replica.api;
 
-import com.atlassian.db.replica.spi.DatabaseCluster;
+import com.atlassian.db.replica.internal.aurora.AuroraClusterDiscovery;
 import com.atlassian.db.replica.spi.ReplicaConsistency;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.function.Supplier;
 
 //TODO: promote to API?
 public class MultiReplicaConsistency implements ReplicaConsistency {
     private final ReplicaConsistency replicaConsistency;
-    private final DatabaseCluster cluster;
+    private final AuroraClusterDiscovery cluster;
 
     public MultiReplicaConsistency(
         ReplicaConsistency replicaConsistency,
-        DatabaseCluster cluster
+        String readerEndpoint,
+        String databaseName  // TODO: can the database name be discovered?
     ) {
         this.replicaConsistency = replicaConsistency;
-        this.cluster = cluster;
+        this.cluster = new AuroraClusterDiscovery(readerEndpoint, databaseName);
     }
 
     @Override
@@ -29,12 +29,7 @@ public class MultiReplicaConsistency implements ReplicaConsistency {
     //    // connections
     @Override
     public boolean isConsistent(Supplier<Connection> replicaSupplier) {
-        try {
-            return cluster.getReplicas().stream().allMatch(replica -> replicaConsistency.isConsistent(() -> {
-                return replica.getConnectionSupplier().get();
-            }));
-        } catch (SQLException throwables) {
-            throw new RuntimeException(throwables);// TODO
-        }
+        return cluster.getReplicas(replicaSupplier).stream()
+            .allMatch(replica -> replicaConsistency.isConsistent(() -> replica.getConnectionSupplier().get()));
     }
 }
