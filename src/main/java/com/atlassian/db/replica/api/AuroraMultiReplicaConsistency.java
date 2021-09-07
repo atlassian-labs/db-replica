72003ff3aeb14a696f3e69eba7c49023501abd13
Tomasz Cieśla <tciesla@atlassian.com>
2021-09-07 15:54:20 +0200

    Added cache to AuroraClusterDiscovery

--- src/main/java/com/atlassian/db/replica/api/AuroraMultiReplicaConsistency.java
+++ src/main/java/com/atlassian/db/replica/api/AuroraMultiReplicaConsistency.java
@@ -1,9 +1,13 @@
 package com.atlassian.db.replica.api;
 
+import com.atlassian.db.replica.internal.Database;
+import com.atlassian.db.replica.internal.NoCacheSuppliedCache;
 import com.atlassian.db.replica.internal.aurora.AuroraClusterDiscovery;
 import com.atlassian.db.replica.spi.ReplicaConsistency;
+import com.atlassian.db.replica.spi.SuppliedCache;
 
 import java.sql.Connection;
+import java.util.Collection;
 import java.util.function.Supplier;
 
 public final class AuroraMultiReplicaConsistency implements ReplicaConsistency {
@@ -12,10 +16,13 @@ public final class AuroraMultiReplicaConsistency implements ReplicaConsistency {
 
     private AuroraMultiReplicaConsistency(
         ReplicaConsistency replicaConsistency,
-        AuroraConnectionDetails auroraConnectionDetails
+        AuroraConnectionDetails auroraConnectionDetails,
+        SuppliedCache<Collection<Database>> discoveredReplicasCache
     ) {
         this.replicaConsistency = replicaConsistency;
-        this.cluster = new AuroraClusterDiscovery(auroraConnectionDetails);
+        this.cluster = AuroraClusterDiscovery.builder()
+            .discoveredReplicasCache(discoveredReplicasCache)
+            .build(auroraConnectionDetails);
     }
 
     @Override
@@ -25,13 +32,20 @@ public final class AuroraMultiReplicaConsistency implements ReplicaConsistency {
 
     @Override
     public boolean isConsistent(Supplier<Connection> replicaSupplier) {
-        return cluster.getReplicas(replicaSupplier).stream()
+        Collection<Database> replicas = cluster.getReplicas(replicaSupplier);
+
+        if (replicas.isEmpty()) {
+            return false;
+        }
+
+        return replicas.stream()
             .allMatch(replica -> replicaConsistency.isConsistent(() -> replica.getConnectionSupplier().get()));
     }
 
     public static final class Builder {
         private ReplicaConsistency replicaConsistency;
         private AuroraConnectionDetails auroraConnectionDetails;
+        private SuppliedCache<Collection<Database>> discoveredReplicasCache = new NoCacheSuppliedCache<>();
 
         public static Builder anAuroraMultiReplicaConsistencyBuilder() {
             return new Builder();
@@ -47,8 +61,22 @@ public final class AuroraMultiReplicaConsistency implements ReplicaConsistency {
             return this;
         }
 
+        /**
+         * It takes #{{@link SuppliedCache}} implementation to store fetched replicas in the cache. This cache should
+         * consider balance between overhead (with no cache we are fetching replicas with every
+         * #{AuroraMultiReplicaConsistency.isConsistent} call) and latency in discovering replica cluster changes.
+         */
+        public Builder discoveredReplicasCache(SuppliedCache<Collection<Database>> discoveredReplicasCache) {
+            this.discoveredReplicasCache = discoveredReplicasCache;
+            return this;
+        }
+
         public ReplicaConsistency build() {
-            return new AuroraMultiReplicaConsistency(replicaConsistency, auroraConnectionDetails);
+            return new AuroraMultiReplicaConsistency(
+                replicaConsistency,
+                auroraConnectionDetails,
+                discoveredReplicasCache
+            );
         }
     }
 
