017442c3426b37e07254848ed0339513cd76488d
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2022-01-26 12:18:07 +0100

    JCES-2328: Add support for `clusterUri` in `AuroraMultiReplicaConsistency`
    By default, the cluster URI is obtained from the provided connection in
    `isConsistent` method. The provided connection doesn't have to use
    cluster URI. The new method allows setting the URI explicitly.

--- src/main/java/com/atlassian/db/replica/internal/aurora/AuroraClusterDiscovery.java
+++ src/main/java/com/atlassian/db/replica/internal/aurora/AuroraClusterDiscovery.java
@@ -1,8 +1,8 @@
 package com.atlassian.db.replica.internal.aurora;
 
 import com.atlassian.db.replica.api.AuroraConnectionDetails;
-import com.atlassian.db.replica.api.jdbc.JdbcUrl;
 import com.atlassian.db.replica.api.Database;
+import com.atlassian.db.replica.api.jdbc.JdbcUrl;
 import com.atlassian.db.replica.internal.NoCacheSuppliedCache;
 import com.atlassian.db.replica.spi.ReplicaConnectionPerUrlProvider;
 import com.atlassian.db.replica.spi.SuppliedCache;
@@ -18,13 +18,16 @@ import static java.util.stream.Collectors.toList;
 public final class AuroraClusterDiscovery {
     private final ReplicaConnectionPerUrlProvider replicaConnectionPerUrlProvider;
     private final SuppliedCache<Collection<Database>> discoveredReplicasCache;
+    private final String clusterUri;
 
     private AuroraClusterDiscovery(
         ReplicaConnectionPerUrlProvider replicaConnectionPerUrlProvider,
-        SuppliedCache<Collection<Database>> discoveredReplicasCache
+        SuppliedCache<Collection<Database>> discoveredReplicasCache,
+        String clusterUri
     ) {
         this.replicaConnectionPerUrlProvider = replicaConnectionPerUrlProvider;
         this.discoveredReplicasCache = discoveredReplicasCache;
+        this.clusterUri = clusterUri;
     }
 
     public Collection<Database> getReplicas(Supplier<Connection> connectionSupplier) {
@@ -51,6 +54,14 @@ public final class AuroraClusterDiscovery {
     }
 
     private AuroraReplicasDiscoverer createDiscoverer(Connection connection) {
+        if (this.clusterUri != null) {
+            return createDiscovererFromClusterUri();
+        } else {
+            return createDiscovererFromConnection(connection);
+        }
+    }
+
+    private AuroraReplicasDiscoverer createDiscovererFromConnection(Connection connection) {
         try {
             final String databaseUrl = connection.getMetaData().getURL();
             final String[] split = databaseUrl.split("/");
@@ -64,6 +75,15 @@ public final class AuroraClusterDiscovery {
         }
     }
 
+    private AuroraReplicasDiscoverer createDiscovererFromClusterUri() {
+        final String[] split = clusterUri.split("/");
+        final String readerEndpoint = split[2];
+        final String databaseName = split[3];
+        return new AuroraReplicasDiscoverer(
+            new AuroraJdbcUrl(AuroraEndpoint.parse(readerEndpoint), databaseName)
+        );
+    }
+
     public static Builder builder() {
         return new Builder();
     }
@@ -71,6 +91,7 @@ public final class AuroraClusterDiscovery {
     public static final class Builder {
         private ReplicaConnectionPerUrlProvider replicaConnectionPerUrlProvider;
         private SuppliedCache<Collection<Database>> discoveredReplicasCache = new NoCacheSuppliedCache<>();
+        private String clusterUri;
 
         public Builder replicaConnectionPerUrlProvider(ReplicaConnectionPerUrlProvider replicaConnectionPerUrlProvider) {
             this.replicaConnectionPerUrlProvider = replicaConnectionPerUrlProvider;
@@ -79,6 +100,11 @@ public final class AuroraClusterDiscovery {
 
         public Builder discoveredReplicasCache(SuppliedCache<Collection<Database>> discoveredReplicasCache) {
             this.discoveredReplicasCache = discoveredReplicasCache;
+            return this;
+        }
+
+        public Builder clusterUri(String clusterUri) {
+            this.clusterUri = clusterUri;
             return this;
         }
 
@@ -94,7 +120,7 @@ public final class AuroraClusterDiscovery {
         }
 
         public AuroraClusterDiscovery build() {
-            return new AuroraClusterDiscovery(replicaConnectionPerUrlProvider, discoveredReplicasCache);
+            return new AuroraClusterDiscovery(replicaConnectionPerUrlProvider, discoveredReplicasCache, clusterUri);
         }
     }
 
