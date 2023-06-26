2fae0a794e5dda0d2be450acb2fb1c006fb83caf
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2023-06-26 09:33:56 +0200

    Fix compatibility with 2.9.0

--- src/main/java/com/atlassian/db/replica/api/DualConnection.java
+++ src/main/java/com/atlassian/db/replica/api/DualConnection.java
@@ -182,7 +182,11 @@ public final class DualConnection implements Connection {
     @Override
     public DatabaseMetaData getMetaData() throws SQLException {
         checkClosed();
-        return connectionProvider.getReadConnection(new RouteDecisionBuilder(Reason.RO_API_CALL)).getMetaData();
+        if (compatibleWithPreviousVersion) {
+            return connectionProvider.getWriteConnection(new RouteDecisionBuilder(Reason.RW_API_CALL)).getMetaData();
+        } else {
+            return connectionProvider.getReadConnection(new RouteDecisionBuilder(Reason.RO_API_CALL)).getMetaData();
+        }
     }
 
     @Override
