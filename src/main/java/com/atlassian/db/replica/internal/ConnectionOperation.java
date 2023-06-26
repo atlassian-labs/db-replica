ef834764f5e6a77844a78bd187d09be5eea48ef7
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2023-06-26 08:03:32 +0200

    Revert "Refactor internal.params package"
    This reverts commit 76ffe3c7cb75e644e6196b3234924aae0800900a.

--- src/main/java/com/atlassian/db/replica/internal/ConnectionOperation.java
+++ src/main/java/com/atlassian/db/replica/internal/ConnectionOperation.java
@@ -1,9 +1,9 @@
-package com.atlassian.db.replica.internal.connection.params;
+package com.atlassian.db.replica.internal;
 
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
-interface ConnectionOperation {
+public interface ConnectionOperation {
     void accept(Connection connection) throws SQLException;
 }
