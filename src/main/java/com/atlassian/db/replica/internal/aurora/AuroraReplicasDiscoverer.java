3ea5bf961e59147f07df6f5a930d70dc0dffa91f
bshehata <bshehata@atlassian.com>
2023-07-21 10:51:15 +1000

    added additional logging to consistency check

--- src/main/java/com/atlassian/db/replica/internal/aurora/AuroraReplicasDiscoverer.java
+++ src/main/java/com/atlassian/db/replica/internal/aurora/AuroraReplicasDiscoverer.java
@@ -43,13 +43,25 @@ public final class AuroraReplicasDiscoverer {
 
     private List<String> fetchReplicasServerIds(Connection connection) throws SQLException {
         List<String> ids = new LinkedList<>();
-        final String sql = "SELECT server_id, round(extract(milliseconds from (now()-last_update_timestamp))) as state_lag_msec, replica_lag_in_msec FROM aurora_replica_status() WHERE session_id != 'MASTER_SESSION_ID' and last_update_timestamp > NOW() - INTERVAL '5 minutes';";
+        final String sql = "SELECT server_id, durable_lsn, current_read_lsn, feedback_xmin, round(extract(milliseconds from (now()-last_update_timestamp))) as state_lag_msec, replica_lag_in_msec " +
+            "FROM aurora_replica_status() " +
+            "WHERE session_id != 'MASTER_SESSION_ID' and last_update_timestamp > NOW() - INTERVAL '5 minutes';";
         try (ResultSet rs =
                  connection.prepareStatement(sql).executeQuery()) {
             while (rs.next()) {
                 String serverId = rs.getString("server_id");
                 int replicaLagInMs = rs.getInt("replica_lag_in_ms");
-                logger.info("Server ID: " + serverId + " Replica lag in ms: " + replicaLagInMs);
+                int durableLsn = rs.getInt("durable_lsn");
+                int currentReadLsn = rs.getInt("current_read_lsn");
+                int feedbackXmin = rs.getInt("feedback_xmin");
+                logger.debug(String.format(
+                    "server_id=%s, replica_lag_in_ms=%d, durable_lsn=%d, current_read_lsn=%d, feedback_xmin=%d",
+                    serverId,
+                    replicaLagInMs,
+                    durableLsn,
+                    currentReadLsn,
+                    feedbackXmin
+                ));
                 ids.add(serverId);
             }
         }
